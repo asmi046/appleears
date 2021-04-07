6448465ed1f52881a25537567bce489941b48625
ruvick <rudikov.web@ya.ru>
2021-04-07 15:36:13 +0300

    Раксидал файлы по папкам

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
-
-</div>
+	<footer id="footer">
+		<div class="inner">
+			<div class="social-box">
+				<a href="#" class="social inst"></a>
+				<a href="#" class="social whatsApp"></a>
+				<a href="#" class="social telegram"></a>
+			</div>
+		</div>
+	</footer>
 	
+</div>
 
 <?php wp_footer(); ?>
 </body>
---
+++ template-parts/header-section.php
@@ -0,0 +1,24 @@
+<header id="header">
+	<a href="#" class="flying-cart">
+		<span class="flying-cart__viz">
+			<span class="cart-contents">1</span>
+		</span> 
+	</a>
+	<div class="inner">
+		<h1 class="main-title">Air Pods 2</h1>
+		<div class="decor-line"></div>
+		<p>
+			С возможностью беспроводной зарядки
+		</p>
+		<p>
+			Поддержка всех функций оригинала
+		</p>
+		<div class="price-box">
+			<span class="db old-price"><span class="inb price">4500 </span><span class="valute inb">₽</span></span>
+			<span class="db new-price">
+				<inb class="inb pirce">2600</inb><span class="valute inb">P</span>
+			</span>
+		</div>
+		<button class="btn">Купить</button>
+	</div>
+</header>
