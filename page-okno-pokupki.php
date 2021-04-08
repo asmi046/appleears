677c0b2cd9abfcdc4097dee20996094a784b001f
ruvick <rudikov.web@ya.ru>
2021-04-08 23:41:42 +0300

    Вывел поля продуктов

--- page-okno-pokupki.php
+++ page-okno-pokupki.php
@@ -8,23 +8,26 @@ Template Post Type: page
 
 ?>
 
-<?php get_template_part('template-parts/header-okno-pokupki');?>
+<?php get_template_part('template-parts/header-okno-pokupki');?> 
 
 <main class="main">
 
 	<section class="slider-section"> 
 		<div class="slider">
-			<div class="slider__item filter">
-				<img src="<?php echo get_template_directory_uri();?>/img/sl-1.jpg" alt="">
-			</div> 
-
-			<div class="slider__item">
-				<img src="<?php echo get_template_directory_uri();?>/img/sl-1.jpg" alt="">
-			</div>
-
-			<div class="slider__item"> 
-				<img src="<?php echo get_template_directory_uri();?>/img/sl-1.jpg" alt="">
-			</div>
+			<?
+			$pict = carbon_get_the_post_meta('offer_picture');
+			if($pict) {
+				$pictIndex = 0;
+				foreach($pict as $item) {
+					?>
+					<div class="slider__item filter">
+						<img src="<?php echo wp_get_attachment_image_src($item['gal_img'], 'full')[0];?>" id = "pict-<? echo empty($item['gal_img_sku'])?$pictIndex:$item['gal_img_sku']; ?>" title = "<? echo $item['gal_img_alt']; ?>" alt="<? echo $item['gal_img_alt']; ?>">
+					</div> 
+					<?
+					$pictIndex++;
+				}
+			}
+			?>
 		</div>
 	</section>
 
@@ -32,8 +35,8 @@ Template Post Type: page
 	<section class="pay-section">
 		<div class="inner">
 			<div class="specific__price d-flex">
-				<div class="specific__new-price">2600 Р</div>
-				<div class="specific__old-price"> 4500 Р</div>
+				<div class="specific__new-price"><?echo carbon_get_post_meta(get_the_ID(),"offer_price"); ?> ₽</div>
+				<div class="specific__old-price"><?echo carbon_get_post_meta(get_the_ID(),"offer_old_price"); ?> ₽</div>
 			</div>
 
 			<div class="specific__act d-flex">
@@ -59,16 +62,7 @@ Template Post Type: page
 			<div class="specific__technic-block">
 				<h2>Технические характеристики</h2>
 				<div class="red-line"></div>
-				<ul>
-					<li>Автоматическое включение и подключение к устройству</li>
-					<li>Простая настройка для работы с любыми устройствами Apple6</li>
-					<li>Быстрый доступ к Siri: достаточно сказать «Привет, Siri» или настроить доступ двойным касанием</li>
-					<li>Двойное касание для начала воспроизведения или переключения	на следующий трек</li>
-					<li>Быстрая подзарядка в футляре</li>
-					<li>Футляр можно заряжать с помощью устройств для беспроводной зарядки или через разъём Lightning</li>
-					<li>Высокое качество звучания музыки и голоса</li>
-					<li>Моментальное переключение с одного устройства на другое</li>
-				</ul>
+				<?echo carbon_get_post_meta(get_the_ID(),"specifications"); ?>
 			</div>
 		</div>
 	</section>
@@ -77,12 +71,7 @@ Template Post Type: page
 		<div class="inner">
 			<h2>Комплектация</h2>
 			<div class="red-line"></div>
-			<ul>
-				<li>AirPods</li>
-				<li>Беспроводной зарядный футляр</li>
-				<li>Кабель Lightning/USB‑A</li>
-				<li>Документация</li>
-			</ul>
+			<?echo carbon_get_post_meta(get_the_ID(),"equipment"); ?>
 		</div>
 	</section>
 
--- themes-fields.php
+++ themes-fields.php
@@ -33,72 +33,20 @@ Container::make( 'theme_options', __( 'Настройки темы', 'crb' ) )
         Field::make( 'text', 'as_telegram', __( 'telegram' ) )
           ->set_width(50),
     ) );
-Container::make('post_meta', 'ultra_product_cr', 'Характеристики товара')
-    ->show_on_post_type(array( 'ultra'))
+Container::make('post_meta', 'excurs_post', 'Доп поля') 
+		->show_on_template('page-okno-pokupki.php') 
       ->add_fields(array(   
-      Field::make('textarea', 'offer_smile_descr', 'Краткое описание')->set_width(100),
-      // Field::make('text', 'offer_name', 'Название товара')->set_width(30),
-      // Field::make('text', 'offer_label', 'Метка на товаре')->set_width(30),
-      Field::make('text', 'offer_manufact', 'Производитель')->set_width(50),
-      // Field::make('text', 'offer_allsearch', 'Все артикулы для поиска')->set_width(50),
-      // Field::make('text', 'offer_siries', 'Серия (для сопутствующих)')->set_width(30),
-
-      // Field::make('text', 'offer_sku', 'Артикул (Базовый)')->set_width(50),
-      Field::make('text', 'offer_nal', 'Наличие на складе')->set_default_value( '0')->set_width(50), 
-        // Field::make('select', 'rev_reiting', 'Оценка' )->add_options( array(
-        //   '0' => '0',
-        //   '1' => '1',
-        //   '2' => '2',
-        //   '3' => '3',
-        //   '4' => '4',
-        //   '5' => '5'
-        // ) )->set_width(20),
-
-      Field::make('text', 'offer_price', 'Цена')->set_width(50),
-      Field::make('text', 'mod_vendor', 'Артикул')->set_width(50),
-      // Field::make('text', 'offer_benefit', 'Выгода')->set_width(50),
-      Field::make( 'complex', 'offer_cherecter', "Характеристики товара табы, левая колонка" )
-      ->add_fields( array(
-        Field::make( 'text', 'tab_name', 'Наименование параметра' )->set_width(50),
-        Field::make( 'text', 'tab_val',  'Значение' )->set_width(50),
-      ) ),
-      Field::make( 'complex', 'offer_cherecter-r', "Характеристики товара табы, правая колонка" )
-      ->add_fields( array(
-        Field::make( 'text', 'tab_name-r', 'Наименование параметра' )->set_width(50),
-        Field::make( 'text', 'tab_val-r',  'Значение' )->set_width(50),
-      ) ),
-      Field::make('rich_text', 'options_text', 'Дополнительные опции')->set_width(100),
-      Field::make('rich_text', 'acses_text', 'Аксесуары')->set_width(100),
-      
-      // Field::make('text', 'offer_old_price', 'Старая цена (Базовая)')->set_width(50),
-      
-      // Field::make( 'complex', 'offer_modification', "Модификация товара" )
-      // ->add_fields( array(
-      //   Field::make('text', 'mod_name', 'Наименование модификации' )->set_width(20),
-      //   Field::make('text', 'mod_sku', 'Артикул модификации')->set_width(20),
-      //   Field::make('text', 'mod_price', 'Цена модификации')->set_width(20),
-      //   Field::make('text', 'mod_old_price', 'Старая цена модификации')->set_width(20),
-      //   Field::make('text', 'mod_picture_id', 'Изображения модификации')->set_width(20),
-      // ) ),
-        
-      Field::make( 'complex', 'offer_picture', "Галерея товара" )
-      ->add_fields( array(
-        Field::make('image', 'gal_img', 'Изображение' )->set_width(30),
-        Field::make('text', 'gal_img_sku', 'ID для модификации')->set_width(30),
-        Field::make('text', 'gal_img_alt', 'alt и title')->set_width(30)        
-      ) ),
-
-      Field::make('complex', 'complex_analogs', 'Ближайшие аналоги')
-        ->set_max(4) // Можно будет выбрать только 5 постов
-      ->add_fields(array(
-        Field::make('image', 'img_analogs', 'Фото')
-          ->set_width(33),
-        Field::make('text', 'price_analogs', 'Цена') 
-          ->set_width(33),
-        Field::make('text', 'link_analogs', 'Ссылка на товар') 
-          ->set_width(33),
-    ))
-
+			Field::make( 'complex', 'offer_picture', "Галерея товара" )
+			->add_fields( array(
+				Field::make('image', 'gal_img', 'Изображение' )->set_width(30),
+				Field::make('text', 'gal_img_sku', 'ID для модификации')->set_width(30),
+				Field::make('text', 'gal_img_alt', 'alt и title')->set_width(30)				
+	) ),
+      Field::make( 'text', 'sticker',  'Стикер' )->set_width(33),
+			Field::make('text', 'offer_price', 'Цена новая')->set_width(33),
+			Field::make('text', 'offer_old_price', 'Цена старая')->set_width(33),
+      Field::make('rich_text', 'specifications', 'Технические характеристики')->set_width(100),
+      Field::make('rich_text', 'equipment', 'Комплектация')->set_width(100),
   ));
 
 ?>
