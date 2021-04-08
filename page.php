91304b878f088237bd229e60b72ce7c911c2b595
ruvick <rudikov.web@ya.ru>
2021-04-08 16:33:48 +0300

    Шаблон тестовой страницы поправил

--- page.php
+++ page.php
@@ -1,23 +1,22 @@
-
 <?php
 
 get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<main id="primary" class="main"> 
 
-	<main id="primary" class="main page site-main"> 
-
-		<section class="content"> 
-			<div class="container">
+	<section class="content"> 
+		<div class="inner">
 
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<h1><?php the_title();?></h1>
-					<?php the_content();?>
-					<?php endwhile;?>
-				<?php endif; ?> 
+				<div class="red-line"></div>
+				<?php the_content();?>
+			<?php endwhile;?>
+		<?php endif; ?> 
 
-			</div>
-		</section>
-	</main>
+	</div>
+</section>
 
-<?php get_footer();
+</main>
+
+<?php get_footer(); 
